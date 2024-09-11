993cb8cb629b733eda1879956c1e5d5d9ce079a2
Rathan <rlandbl@gmail.com>
2024-09-11 08:06:44 -0500

    disable resources

--- ec2test/main.tf
+++ ec2test/main.tf
@@ -3,12 +3,14 @@ provider "aws" {
 }
 
 resource "aws_vpc" "myvpc" {
+  settings.spacelift.workspace_enabled: false
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "myvpc"
   }
 }
 resource "aws_subnet" "mysubnet" {
+  settings.spacelift.workspace_enabled: false
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "10.0.1.0/24"
   tags = {
@@ -17,6 +19,7 @@ resource "aws_subnet" "mysubnet" {
 }
 
 resource "aws_instance" "ubuntu2204" {
+  settings.spacelift.workspace_enabled: false
   ami           = "ami-080e1f13689e07408"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.mysubnet.id
